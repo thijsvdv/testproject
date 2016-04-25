4a51c6ec2d9fa1e8a9a3034f1353c4afdf467011
Thijs Van de Velde <thijs@Thijss-MacBook-Pro.local>
2016-04-25 20:48:39 +0200

    Remove default example

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,31 +8,10 @@
 
 import React from 'react';
 
-// Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
-import 'sanitize.css/sanitize.css';
-
-import Img from 'Img';
-import Footer from 'Footer';
-import Banner from './banner-metal.jpg';
-import A from 'A';
-
-import styles from './styles.css';
-
 function App(props) {
   return (
-    <div className={ styles.wrapper }>
-      <A
-        className={ styles.logoWrapper }
-        href="https://twitter.com/mxstbr"
-      >
-        <Img
-          className={ styles.logo }
-          src={ Banner }
-          alt="react-boilerplate - Logo"
-        />
-      </A>
+    <div>
       { props.children }
-      <Footer />
     </div>
   );
 }
--- app/selectors/tests/selectLocationSelector.test.js
+++ app/selectors/tests/selectLocationSelector.test.js
@@ -2,8 +2,6 @@ import selectLocationSelector from '../selectLocationSelector';
 import { fromJS } from 'immutable';
 import expect from 'expect';
 
-const selector = selectLocationSelector();
-
 describe('selectLocationSelector', () => {
   it('should select the route as a plain JS object', () => {
     const route = fromJS({
@@ -12,6 +10,6 @@ describe('selectLocationSelector', () => {
     const mockedState = fromJS({
       route,
     });
-    expect(selector(mockedState)).toEqual(route.toJS());
+    expect(selectLocationSelector(mockedState)).toEqual(route.toJS());
   });
 });
